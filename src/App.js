c99506ec92768c6a0d5cc21e7c8b195d55711c42
bensam <ben.ss@inapp.com>
2022-10-14 16:17:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,114 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(1);
+
+  const [but1, setBut1] = useState({ value: "", isDisabled: false });
+  const [but2, setBut2] = useState({ value: "", isDisabled: false });
+  const [but3, setBut3] = useState({ value: "", isDisabled: false });
+  const [but4, setBut4] = useState({ value: "", isDisabled: false });
+  const [but5, setBut5] = useState({ value: "", isDisabled: false });
+  const [but6, setBut6] = useState({ value: "", isDisabled: false });
+  const [but7, setBut7] = useState({ value: "", isDisabled: false });
+  const [but8, setBut8] = useState({ value: "", isDisabled: false });
+  const [but9, setBut9] = useState({ value: "", isDisabled: false });
+
+  function putValue(setBut) {
+    let value = "";
+    if (count % 2 === 0) {
+      value = "O";
+    } else {
+      value = "X";
+    }
+
+    setBut({ value: value, isDisabled: true });
+    setCount(count + 1);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <tr>
+          <td>
+            <button
+              onClick={() => putValue(setBut1)}
+              disabled={but1.isDisabled}
+            >
+              {but1.value}
+            </button>
+          </td>
+          <td>
+            <button
+              onClick={() => putValue(setBut2)}
+              disabled={but2.isDisabled}
+            >
+              {but2.value}
+            </button>
+          </td>
+          <td>
+            <button
+              onClick={() => putValue(setBut3)}
+              disabled={but3.isDisabled}
+            >
+              {but3.value}
+            </button>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <button
+              onClick={() => putValue(setBut4)}
+              disabled={but4.isDisabled}
+            >
+              {but4.value}
+            </button>
+          </td>
+          <td>
+            <button
+              onClick={() => putValue(setBut5)}
+              disabled={but5.isDisabled}
+            >
+              {but5.value}
+            </button>
+          </td>
+          <td>
+            <button
+              onClick={() => putValue(setBut6)}
+              disabled={but6.isDisabled}
+            >
+              {but6.value}
+            </button>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <button
+              onClick={() => putValue(setBut7)}
+              disabled={but7.isDisabled}
+            >
+              {but7.value}
+            </button>
+          </td>
+          <td>
+            <button
+              onClick={() => putValue(setBut8)}
+              disabled={but8.isDisabled}
+            >
+              {but8.value}
+            </button>
+          </td>
+          <td>
+            <button
+              onClick={() => putValue(setBut9)}
+              disabled={but9.isDisabled}
+            >
+              {but9.value}
+            </button>
+          </td>
+        </tr>
+      </table>
+      <div></div>
     </div>
   );
 }
